265b8de517db90464deeea89084ee786ccd09453
Amark19 <amarkhamkar6@gmail.com>
2022-03-30 19:43:11 +0530

    done

--- src/Component/popSpec.jsx
+++ src/Component/popSpec.jsx
@@ -1,4 +1,3 @@
-import { width } from 'dom-helpers';
 import React,{useEffect,useState} from 'react'
 
 
@@ -8,53 +7,50 @@ const PopSpec = () => {
     const res=await fetch("https://apidynamicpge.herokuapp.com/popSpec?format=json");
     const actualData=await res.json();
     setData(actualData);
-    console.log(actualData);
   }
     useEffect(() => {
         getDataSpec();
     }, []);
-
-    const  vals = [];
-    let k = 0;
-    for (var i = 0; i < Data.length/3; i++) {
-        a = []
-        for(var j=0;j<3;j++){
-        if(k<Data.length){
-        a.push(Data.spec[k])
-        k++;
+    //code to shuffle Data
+    const shuffle = (array) => {
+        let currentIndex = array.length, temporaryValue, randomIndex;
+        while (0 !== currentIndex) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
         }
-        
-    }
-    vals.push(a);
-    }
-    console.log(vals);
+        return array;
+      }
+    const shuffledData=shuffle(Data);
   return (
     <div>
     <hr className="my-4 bg-dark container"
     />
    <h3 className="my-4 text-center fw-bold">Popular Specializations</h3>
    <p className="text-center fs-6">Below are some of the popular Specializations of computer science</p>
-   <div className="allSpec">
+   <div className="container">
+     <div className="row mx-4 my-4">
    {
-   Data.map((onePop)=>{
-return(
-        <div className="row">
-        <div className="col-md-4">
-        <div className="txt" style={{width:'100px',height:'100px',backgroundColor:'grey'}}>
-      <p className="fs-6 fw-bold">{onePop.spec}</p>
-      
-        }
-      </div>
-        </div>
-
-        </div>
-        
-
-)}
-)}  
+     
+    shuffledData.map((oneSpec)=>{
+         return(
+           <div className="col-md-3">
+           <p className='text-center py-4' style={{backgroundColor: '#dcdcdc',
+    borderRadius:' 5px',
+    width: '220px',
+    height: '80px',
+    top:'20%'
+}}>{oneSpec.spec}</p>
+           </div>
+         )
+       })
+}
+</div>
 </div>
    </div>
   )
 }
 
-export default PopSpec
+export default PopSpec;
